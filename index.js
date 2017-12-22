a2c3d23a909f046b0fb5224547a8547283bffe94
Humza Joseph <humza1born@gmail.com>
2017-12-22 01:09:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,41 +1,29 @@
 const app = "I don't do much."
-
+var kittens= ["milo","Otis","Garfield"];
 function kittens(name){
-  var kittens= ["milo","Otis","Garfield"];
   return kittens;
 }
-
-kittens("milo","Otis","Garfield");
 
 function destructivelyAppendKitten(name) {
   kittens.push('Ralph');
   return kittens;
 }
 
-kittens("milo","Otis","Garfield");
-
 function destructivelyPrependKitten(name) {
   kittens.unshift("Bob");
   return kittens;
 }
-
-kittens("milo","Otis","Garfield");
 
 function destructivelyRemoveLastKitten(name) {
   kittens.pop();
   return kittens;
 }
 
-kittens("milo","Otis","Garfield");
-
 function destructivelyRemoveFirstKitten(name) {
   kittens.shift();
   return kittens;
 }
 
-kittens("milo","Otis","Garfield");
-
 function appendKitten(name) {
-Kitten("Broom");
-return Kitten;
+  
 }
